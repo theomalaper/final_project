6496562aa6835a9197532723abe2e563a133cb01
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-26 18:16:28 -0400

    switch from a class component to a function component with hooks

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const Pin = ({ text }) => <div>{text}</div>;
  
-class Map extends Component {
-  static defaultProps = {
-    center: {
-      lat: 47.5260,
-      lng: 7.2551
-    },
-    zoom: 3.8
-  };
+export default function Map(props) {
+  const [center, setCenter] = useState({lat: 47.5260, lng: 7.2551 });
+  const [zoom, setZoom] = useState(3.8);
 
-  render() {
-    return (
-      // Important! Always set the container height explicitly
-      <div style={{ height: '50vh', width: '45%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}` }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-            lat={48.8566}
-            lng={2.3522}
-            text="Paris"
-          />
-        </GoogleMapReact>
-      </div>
-    );
-  }
+  // static defaultProps = {
+  //   center: {
+  //     lat: 47.5260,
+  //     lng: 7.2551
+  //   },
+  //   zoom: 3.8
+  // };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: '50vh', width: '45%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`, language: 'en' }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <Pin
+          lat={48.8566}
+          lng={2.3522}
+          text="Paris"
+        />
+      </GoogleMapReact>
+    </div>
+  );
 }
- 
-export default Map;
