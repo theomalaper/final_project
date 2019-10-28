caeb1fe34d496646a0acd3395e1c5fe355a92c62
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-28 00:56:48 -0400

    implement  basic static trippage

--- client/src/components/TripPage.js
+++ client/src/components/TripPage.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import {
   useParams
 } from "react-router-dom";
 import Map from './Map';
+import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 
 
-export default function CityPage() {
+export default function TripPage() {
   const { id } = useParams();
 
   return (
-    <div>
-      <h1>Trip</h1>
-      <h2>ID: {id}</h2>
-      <h1>Google Maps</h1>
-      <Map />
-    </div>
+    <Fragment>
+      <header>
+        <h1>Your Trip</h1>
+        <Map />
+      </header>
+      <section>
+        <h2>Paris</h2>
+        <ul>
+          <li>Activity 1</li>
+          <li>Activity 2</li>
+        </ul>
+        <ButtonToolbar aria-label="Toolbar with button groups">
+          <ButtonGroup className="mr-2" aria-label="First group">
+            <Button>A</Button>
+            <Button>H</Button>
+            <Button>Hs</Button>
+          </ButtonGroup>
+          <ButtonGroup className="mr-2" aria-label="Second group">
+            <Button>P</Button>
+            <Button>T</Button>
+            <Button>7</Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+      </section>
+      <section>
+        <h2>Summary</h2>
+        <ul>
+          <li>Cities explored: 1</li>
+          <li>Duration: 7 days</li>
+          <li>Cost: 1000$</li>
+        </ul>
+      </section>
+    </Fragment>
   );
 };
