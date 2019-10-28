329c0908d20b9aa4d7c50530b5813a966f3efde7
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-28 00:30:05 -0400

    pin  now renders an image

--- client/src/components/Pin.js
+++ client/src/components/Pin.js
@@ -2,14 +2,9 @@ import React from 'react';
 import './Pin.scss';
 
 export default function Pin(props) {
-    const { color, name, id } = props;
     return (
       <div>
-        <div
-          className="pin"
-          style={{ backgroundColor: color, cursor: 'pointer' }}
-          title={name}
-        />
+        <img className="pin" src="https://cdn.shopify.com/s/files/1/0870/9752/products/airbnb-round_ce25d804-7533-4fdd-b4ef-cd95c9dedf3b_800x.png?v=1550175979" />
       </div>
     );
   };
