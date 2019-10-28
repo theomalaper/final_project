a04fb13059f40b7843de972f3e04bb242f7365ff
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-28 12:00:03 -0400

    implement react-bootstrap to lists and buttons

--- client/src/components/TripPage.js
+++ client/src/components/TripPage.js
@@ -2,8 +2,9 @@ import React, { Fragment } from 'react';
 import {
   useParams
 } from "react-router-dom";
+import { Button, ButtonGroup, ButtonToolbar, ListGroup } from 'react-bootstrap';
 import Map from './Map';
-import { Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
+import './TripPage.scss';
 
 
 export default function TripPage() {
@@ -11,36 +12,42 @@ export default function TripPage() {
 
   return (
     <Fragment>
-      <header>
-        <h1>Your Trip</h1>
+      <header className="map">
         <Map />
       </header>
-      <section>
-        <h2>Paris</h2>
-        <ul>
-          <li>Activity 1</li>
-          <li>Activity 2</li>
-        </ul>
+      <h1 className="trip-title">CHRISTMAS 2019</h1>
+      <section className="block">
+        <h3 className="city-title">Paris</h3>
+        <ListGroup variant="flush">
+          <ListGroup.Item className="content">20/12/2019 - 27/12/2019 (7 days)</ListGroup.Item>
+          <ListGroup.Item className="content">Activities
+            <ul>
+              <li>Activity 1</li>
+              <li>Activity 2</li>
+            </ul>
+          </ListGroup.Item>
+          <ListGroup.Item className="content">Cost: 1000$</ListGroup.Item>
+        </ListGroup>
         <ButtonToolbar aria-label="Toolbar with button groups">
-          <ButtonGroup className="mr-2" aria-label="First group">
-            <Button>A</Button>
-            <Button>H</Button>
-            <Button>Hs</Button>
+          <ButtonGroup className="mr-2 buttons" aria-label="First group">
+            <Button disabled variant="secondary">A</Button>
+            <Button disabled variant="light">H</Button>
+            <Button disabled variant="light">Hs</Button>
           </ButtonGroup>
-          <ButtonGroup className="mr-2" aria-label="Second group">
-            <Button>P</Button>
-            <Button>T</Button>
-            <Button>7</Button>
+          <ButtonGroup className="mr-2 buttons" aria-label="Second group">
+            <Button disabled variant="light">B</Button>
+            <Button disabled variant="secondary">T</Button>
+            <Button disabled variant="light">P</Button>
           </ButtonGroup>
         </ButtonToolbar>
       </section>
-      <section>
-        <h2>Summary</h2>
-        <ul>
-          <li>Cities explored: 1</li>
-          <li>Duration: 7 days</li>
-          <li>Cost: 1000$</li>
-        </ul>
+      <section className="block">
+        <h3 className="summary-title">Summary</h3>
+        <ListGroup variant="flush">
+          <ListGroup.Item className="content">Cities explored: 1</ListGroup.Item>
+          <ListGroup.Item className="content">Duration: 7 days</ListGroup.Item>
+          <ListGroup.Item className="content">Cost: 1000$</ListGroup.Item>
+        </ListGroup>
       </section>
     </Fragment>
   );
