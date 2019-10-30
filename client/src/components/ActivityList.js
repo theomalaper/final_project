691aa3c2222882b3e5f7133c44559b81cd4336d9
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-30 10:29:09 -0400

    create the component and implement ti to render an activity block

---
+++ client/src/components/ActivityList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ActivityItem from "./ActivityItem";
+
+export default function ActivityList(props) {
+  const { activities } = props;
+  if (!activities) {
+    return null
+  } 
+  return (activities.map(act =>
+      <ActivityItem 
+        key={act.id}
+        name={act.name}
+        description={act.description}
+        image={act.activity_image}
+        link_names={act.link_names}
+        link_types={act.link_types}
+        link_urls={act.link_urls}
+      />
+  ));
+};
