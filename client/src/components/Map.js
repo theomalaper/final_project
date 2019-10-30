3d901d9a2bf7c7cc64fee2ca9424d5c97ae587d0
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-30 15:02:31 -0400

    pass coordinates props to the Pin component to display the pins of the cities already added in a trip

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -5,12 +5,9 @@ import Pin from './Pin';
 // const Pin = ({ text }) => <div>{text}</div>;
  
 export default function Map(props) {
-  const { cities, city } = props;
+  const { citiesInTrip, city } = props;
 
-  const [center, setCenter] = useState({lat: 47.5260, lng: 7.2551 });
-  const [zoom, setZoom] = useState(3.8);
-
-  if (city) {
+  if (city && citiesInTrip) {
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '50vh', width: '100%' }}>
@@ -20,9 +17,15 @@ export default function Map(props) {
           defaultZoom={city[0].zoom}
         >
           <Pin
-            lat={48.8566}
-            lng={2.3522}
+            lat={city[0].coordinate_latitude}
+            lng={city[0].coordinate_longitude}
           />
+          {citiesInTrip.map(city =>
+          <Pin
+            lat={city.coordinate_latitude}
+            lng={city.coordinate_longitude}
+          />
+          )}
         </GoogleMapReact>
       </div>
     )
