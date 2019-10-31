698d6f7f4786ea1b708f6c2a58ee36b85cbf5eca
Julien Atanassian <julien.atanassian@gmail.com>
2019-10-31 14:33:32 -0400

    if the first transport query returns null, check for the same transport but reverse ending and starting cities

--- mock_api/routes/transport.js
+++ mock_api/routes/transport.js
@@ -5,7 +5,13 @@ module.exports = knex => {
   router.get('/bus/:starting_city/:ending_city', function(req, res, next) {
     knex('transports').avg('price').where('transport_type_id', 1).andWhere('starting_city', req.params.starting_city).andWhere('ending_city', req.params.ending_city)
       .then(result => {
+        if (result[0].avg === null) {
+          knex('transports').avg('price').where('transport_type_id', 1).andWhere('starting_city', req.params.ending_city).andWhere('ending_city', req.params.starting_city)
+          .then(result => res.json(result[0]))
+          .catch(err => console.log(err))
+        } else {
         res.json(result[0]);
+        }
       })
       .catch(error => {
         console.log(error);
@@ -16,7 +22,13 @@ module.exports = knex => {
   router.get('/train/:starting_city/:ending_city', function(req, res, next) {
     knex('transports').avg('price').where('transport_type_id', 2).andWhere('starting_city', req.params.starting_city).andWhere('ending_city', req.params.ending_city)
       .then(result => {
+        if (result[0].avg === null) {
+          knex('transports').avg('price').where('transport_type_id', 2).andWhere('starting_city', req.params.ending_city).andWhere('ending_city', req.params.starting_city)
+          .then(result => res.json(result[0]))
+          .catch(err => console.log(err))
+        } else {
         res.json(result[0]);
+        }
       })
       .catch(error => {
         console.log(error);
@@ -27,7 +39,13 @@ module.exports = knex => {
   router.get('/plane/:starting_city/:ending_city', function(req, res, next) {
     knex('transports').avg('price').where('transport_type_id', 3).andWhere('starting_city', req.params.starting_city).andWhere('ending_city', req.params.ending_city)
       .then(result => {
+        if (result[0].avg === null) {
+          knex('transports').avg('price').where('transport_type_id', 3).andWhere('starting_city', req.params.ending_city).andWhere('ending_city', req.params.starting_city)
+          .then(result => res.json(result[0]))
+          .catch(err => console.log(err))
+        } else {
         res.json(result[0]);
+        }
       })
       .catch(error => {
         console.log(error);
